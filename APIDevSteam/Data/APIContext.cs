715c84f41390180f519c7b9c9e685720739d4f3a
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-04-16 15:55:47 -0300

    Fazendo as migrations

--- APIDevSteam/Data/APIContext.cs
+++ APIDevSteam/Data/APIContext.cs
@@ -9,13 +9,24 @@ namespace APIDevSteam.Data
         public APIContext(DbContextOptions<APIContext> options) : base(options)
         {
         }
-
         // DbSet
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<JogoCategoria> JogosCategorias { get; set; }
+        public DbSet<JogoMidia> jogoMidias { get; set; }
+
+
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Tabelas
+            builder.Entity<Jogo>().ToTable("Jogos");
+            builder.Entity<Categoria>().ToTable("Categorias");
+            builder.Entity<JogoCategoria>().ToTable("JogosCategorias");
+            builder.Entity<JogoMidia>().ToTable("JogoMidias");
+
 
         }
     }
